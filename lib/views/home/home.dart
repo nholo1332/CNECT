8a2d9d1dd65102da99540958d9ed018f07324e57
Noah Holoubek <noahholo@icloud.com>
2021-02-12 15:13:24 -0600

    Create home.dart

---
+++ lib/views/home/home.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatefulWidget {
+  HomeView();
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}

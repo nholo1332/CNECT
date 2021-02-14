6ef5ea339bcf13bf9081b264fb90e963f82ad304
Noah Holoubek <noahholo@icloud.com>
2021-02-14 12:13:28 -0600

    Initialize GooglePlaces

--- lib/providers/globals.dart
+++ lib/providers/globals.dart
@@ -1,5 +1,7 @@
 import 'package:cnect/models/user.dart';
+import 'package:google_place/google_place.dart';
 
 class Globals {
   static UserClass currentUser;
+  static final googlePlace = GooglePlace('AIzaSyCzHWH_mROmn_cptAWjCMKGjtCEHnqSnlo');
 }

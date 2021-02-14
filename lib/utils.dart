8301c97b0e9d146b00d4e266044b194f77fa7f9f
Noah Holoubek <noahholo@icloud.com>
2021-02-13 20:33:12 -0600

    Create Utils

---
+++ lib/utils.dart
@@ -0,0 +1,9 @@
+class Utils {
+
+  static bool isSameDate(DateTime date1, DateTime date2) {
+    return date1.day == date2.day &&
+        date1.month == date2.month &&
+        date1.year == date2.year;
+  }
+
+}

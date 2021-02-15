bfedf853182c1fd97726c6f726a49f6c7a7bf6e9
Noah Holoubek <noahholo@icloud.com>
2021-02-15 13:46:03 -0600

    Add option to customize horizontal margin for large rounded button

--- lib/widgets/largeRoundedButton.dart
+++ lib/widgets/largeRoundedButton.dart
@@ -3,20 +3,23 @@ import 'package:flutter/material.dart';
 class LargeRoundedButton extends StatelessWidget {
 
   LargeRoundedButton({
+    this.horizontalMargin = 50,
     @required this.backgroundColor,
     @required this.childWidget,
     @required this.onTapAction,
   });
 
+  final double horizontalMargin;
   final Color backgroundColor;
   final Widget childWidget;
   final GestureTapCallback onTapAction;
 
   @override
   Widget build(BuildContext context) {
+    // Build button and return Widget
     return Container(
       height: 50,
-      margin: EdgeInsets.symmetric(horizontal: 50),
+      margin: EdgeInsets.symmetric(horizontal: horizontalMargin),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(50),
         color: backgroundColor,

4e1e178f133986cfbcb0209abddd49a61283e34b
Noah Holoubek <noahholo@icloud.com>
2021-02-15 11:36:16 -0600

    Add optional location name

--- lib/models/location.dart
+++ lib/models/location.dart
@@ -2,22 +2,26 @@ class LocationData {
   String address;
   double long;
   double lat;
+  String name;
 
   LocationData({
     this.address,
     this.long,
     this.lat,
+    this.name,
   });
 
   LocationData.fromJson(Map<String, dynamic> data) {
     address = data['address'];
     long = data['long'];
     lat = data['lat'];
+    name = data['name'];
   }
 
   Map<String, dynamic> toJson() => {
     'address': address,
     'long': long,
     'lat': lat,
+    'name': name,
   };
 }

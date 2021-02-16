e4bd43bc76482505df633aced183500ef2dc83b1
Noah Holoubek <noahholo@icloud.com>
2021-02-16 12:24:22 -0600

    Create global variable to hold list of businesses

--- lib/providers/globals.dart
+++ lib/providers/globals.dart
@@ -1,7 +1,9 @@
+import 'package:cnect/models/business.dart';
 import 'package:cnect/models/user.dart';
 import 'package:google_place/google_place.dart';
 
 class Globals {
   static UserClass currentUser;
   static final googlePlace = GooglePlace('AIzaSyCzHWH_mROmn_cptAWjCMKGjtCEHnqSnlo');
+  static Map<String, List<Business>> businesses = {};
 }

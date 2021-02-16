b0471a2fe3db6f369f9073ee5f65dcfe77396877
Noah Holoubek <noahholo@icloud.com>
2021-02-15 21:16:23 -0600

    Add map and setup reset zoom button

--- lib/views/map/map.dart
+++ lib/views/map/map.dart
@@ -1,4 +1,7 @@
+import 'package:cnect/models/community.dart';
+import 'package:cnect/providers/globals.dart';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapView extends StatefulWidget {
   MapView();
@@ -10,13 +13,84 @@ class MapView extends StatefulWidget {
 class _MapViewState extends State<MapView> {
   final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
 
+  GoogleMapController mapController;
+
+  LatLng communityCenter = LatLng(0, 0);
+
+  List<Community> communities;
+
+  Community selectedCommunity;
+
+  void initState() {
+    super.initState();
+    communities = Globals.currentUser.communities;
+    selectedCommunity = communities.first;
+    communityCenter = new LatLng(selectedCommunity.location.lat, selectedCommunity.location.long);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.my_location),
+        onPressed: () {
+          mapController.animateCamera(
+            CameraUpdate.zoomTo(15),
+          );
+        },
+      ),
       body: Container(
-        child: Text('Test'),
+        child: Stack(
+          alignment: Alignment.topCenter,
+          children: [
+            GoogleMap(
+              onMapCreated: _onMapCreated,
+              myLocationButtonEnabled: false,
+              zoomGesturesEnabled: true,
+              zoomControlsEnabled: true,
+              initialCameraPosition: CameraPosition(
+                target: communityCenter,
+                zoom: 15,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 45),
+              child: Card(
+                child: Container(
+                  width: 175,
+                  height: 50,
+                  alignment: Alignment.centerRight,
+                  padding: EdgeInsets.only(
+                    left: 10,
+                    right: 10,
+                  ),
+                  child: DropdownButton<Community>(
+                    value: selectedCommunity,
+                    isExpanded: true,
+                    items: communities.map((Community value) {
+                      return DropdownMenuItem<Community>(
+                        value: value,
+                        child: Text(value.name),
+                      );
+                    }).toList(),
+                    onChanged: (value) {
+                      setState(() {
+                        selectedCommunity = value;
+                        communityCenter = new LatLng(selectedCommunity.location.lat, selectedCommunity.location.long);
+                      });
+                    },
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  void _onMapCreated(GoogleMapController controller)async  {
+    mapController = controller;
   }
 
 }

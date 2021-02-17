968a2a5f78a34290e631302ec09431bd79261536
Noah Holoubek <noahholo@icloud.com>
2021-02-17 17:00:08 -0600

    Create globals for stored community and followed businesses events

--- lib/providers/globals.dart
+++ lib/providers/globals.dart
@@ -1,4 +1,5 @@
 import 'package:cnect/models/business.dart';
+import 'package:cnect/models/event.dart';
 import 'package:cnect/models/user.dart';
 import 'package:google_place/google_place.dart';
 
@@ -6,4 +7,6 @@ class Globals {
   static UserClass currentUser;
   static final googlePlace = GooglePlace('AIzaSyCzHWH_mROmn_cptAWjCMKGjtCEHnqSnlo');
   static Map<String, List<Business>> businesses = {};
+  static Map<String, List<Event>> communityEvents = {};
+  static List<Business> followedBusinessesEvents = [];
 }

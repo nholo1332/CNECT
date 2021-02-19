aa8f85359ef8e71d7efe0581f6b57775846428a2
Noah Holoubek <noahholo@icloud.com>
2021-02-19 13:49:15 -0600

    Design profile view

--- lib/views/profile/profile.dart
+++ lib/views/profile/profile.dart
@@ -1,3 +1,9 @@
+import 'package:cnect/main.dart';
+import 'package:cnect/models/community.dart';
+import 'package:cnect/providers/backend.dart';
+import 'package:cnect/providers/globals.dart';
+import 'package:cnect/views/privacyPolicy/privacyPolicy.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ProfileView extends StatefulWidget {
@@ -9,6 +15,25 @@ class ProfileView extends StatefulWidget {
 
 class _ProfileViewState extends State<ProfileView> {
   final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+
+  String newName;
+  String newDescription;
+
+  bool isSaving = false;
+
+  TextEditingController nameTextController;
+  TextEditingController descriptionTextController;
+
+  FirebaseAuth auth = FirebaseAuth.instance;
+
+  @override
+  void initState() {
+    super.initState();
+    newName = Globals.currentUser.name;
+    newDescription = Globals.currentUser.description;
+    nameTextController = new TextEditingController(text: newName);
+    descriptionTextController = new TextEditingController(text: newDescription);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -83,7 +108,133 @@ class _ProfileViewState extends State<ProfileView> {
                       padding: EdgeInsets.all(30),
                       child: Column(
                         children: <Widget>[
-
+                          Container(
+                            width: double.infinity,
+                            child: Card(
+                              child: Column(
+                                children: [
+                                  ListTile(
+                                    title: Text('Profile'),
+                                    subtitle: Text('Edit your publicly viewable information'),
+                                  ),
+                                  Padding(
+                                    padding: EdgeInsets.only(
+                                      left: 20,
+                                      right: 20,
+                                    ),
+                                    child: TextField(
+                                      controller: nameTextController,
+                                      enabled: !isSaving,
+                                      decoration: InputDecoration(
+                                        labelText: 'Name',
+                                        border: OutlineInputBorder(
+                                          borderRadius: BorderRadius.circular(8),
+                                        ),
+                                      ),
+                                      onChanged: (String value) {
+                                        setState(() {
+                                          this.newName = value;
+                                        });
+                                      },
+                                    ),
+                                  ),
+                                  SizedBox(height: 12),
+                                  Padding(
+                                    padding: EdgeInsets.only(
+                                      left: 20,
+                                      right: 20,
+                                    ),
+                                    child: TextField(
+                                      controller: descriptionTextController,
+                                      enabled: !isSaving,
+                                      maxLines: 3,
+                                      decoration: InputDecoration(
+                                        labelText: 'Bio',
+                                        border: OutlineInputBorder(
+                                          borderRadius: BorderRadius.circular(8),
+                                        ),
+                                      ),
+                                      onChanged: (String value) {
+                                        setState(() {
+                                          this.newDescription = value;
+                                        });
+                                      },
+                                    ),
+                                  ),
+                                  SizedBox(height: 15),
+                                  OutlineButton(
+                                    child: Text('Update'),
+                                    onPressed: isSaving
+                                        ? null
+                                        : () => saveName(),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 15),
+                          buildCommunitiesCard(context),
+                          SizedBox(height: 15),
+                          Card(
+                            child: Column(
+                              children: [
+                                ListTile(
+                                  title: Text('Logout'),
+                                  subtitle: Text('Logout from your account'),
+                                ),
+                                OutlineButton(
+                                  child: Text(
+                                    'Logout',
+                                    style: TextStyle(
+                                      color: Colors.red,
+                                    ),
+                                  ),
+                                  onPressed: () => logout(),
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(height: 15),
+                          Card(
+                            child: Column(
+                              children: [
+                                ListTile(
+                                  title: Text('Info'),
+                                  subtitle: Text('Version' + Globals.packageInfo.version + ' - Build ' + Globals.packageInfo.buildNumber),
+                                ),
+                                Center(
+                                  child: ConstrainedBox(
+                                    constraints: BoxConstraints(
+                                      minWidth: 120,
+                                      minHeight: 120,
+                                      maxWidth: 120,
+                                      maxHeight: 120,
+                                    ),
+                                    child: Image.asset(
+                                      'assets/images/logos/logo.png',
+                                      fit: BoxFit.fitHeight,
+                                    ),
+                                  ),
+                                ),
+                                Text(
+                                  'Connecting local professionals with the community',
+                                  textAlign: TextAlign.center,
+                                ),
+                                SizedBox(height: 5),
+                                OutlineButton(
+                                  child: Text('Privacy Policy'),
+                                  onPressed: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => PrivacyPolicyView(),
+                                      ),
+                                    );
+                                  },
+                                ),
+                              ],
+                            ),
+                          ),
                         ],
                       ),
                     ),
@@ -95,6 +246,93 @@ class _ProfileViewState extends State<ProfileView> {
         ),
       ),
     );
+  }
+
+  logout() {
+    auth.signOut().then((value) {
+      Globals.businesses = {};
+      Globals.communityEvents = {};
+      Globals.followedBusinessesEvents = [];
+      Globals.communityAnnouncements = {};
+      Globals.selectedCommunity = null;
+      Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(
+          builder: (BuildContext context) => SplashScreen(),
+        ),
+        (route) => false
+      );
+    }).catchError((error) {
+      scaffoldKey.currentState.showSnackBar(
+        SnackBar(
+          content: Text('Failed to logout'),
+        ),
+      );
+    });
+  }
+
+  buildCommunityListItem(Community community) {
+    return ListTile(
+      leading: Icon(Icons.location_city),
+      title: Text(community.name),
+      subtitle: Text(community.description),
+    );
+  }
+
+  Widget buildCommunitiesCard(BuildContext context) {
+    List<Widget> widgets = [
+      ListTile(
+        title: Text('Communities'),
+        subtitle: Text('Communities you have joined'),
+      ),
+    ];
+    if ( Globals.currentUser.communities.length > 0 ) {
+      widgets.addAll(Globals.currentUser.communities.map((c) => buildCommunityListItem(c)));
+    } else {
+      widgets.add(
+        Padding(
+          padding: EdgeInsets.only(bottom: 12),
+          child: Center(
+            child: Text('You have not joined any communities'),
+          ),
+        ),
+      );
+    }
+    return Container(
+      width: double.infinity,
+      child: Card(
+        child: Column(
+          children: widgets,
+        ),
+      ),
+    );
+  }
+
+  void saveName() {
+    setState(() {
+      isSaving = true;
+    });
+    Backend.saveName(newName, newDescription).then((value) {
+      setState(() {
+        isSaving = false;
+      });
+      Globals.currentUser.name = newName;
+      Globals.currentUser.description = newDescription;
+      scaffoldKey.currentState.showSnackBar(
+        SnackBar(
+          content: Text('Saved profile'),
+        ),
+      );
+    }).catchError((error) {
+      setState(() {
+        isSaving = false;
+      });
+      scaffoldKey.currentState.showSnackBar(
+        SnackBar(
+          content: Text('Failed to save profile'),
+        ),
+      );
+    });
   }
 
 }

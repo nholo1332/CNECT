6483534bd2a6801c757f3df263e35bc7ca208e40
Noah Holoubek <noahholo@icloud.com>
2021-02-18 23:22:21 -0600

    Add extra list tiles for routes in profile bottom sheet

--- lib/widgets/profileBottomSheet.dart
+++ lib/widgets/profileBottomSheet.dart
@@ -21,96 +21,107 @@ class ProfileBottomSheet {
             topRight: Radius.circular(60),
           ),
           child: Container(
-            height: 230,
+            height: 320,
             color: Theme.of(context).scaffoldBackgroundColor,
             child: Container(
-                decoration: BoxDecoration(
-                  color: Theme.of(context).bottomSheetTheme.backgroundColor,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(20),
-                    topRight: Radius.circular(20),
-                  ),
+              decoration: BoxDecoration(
+                color: Theme.of(context).bottomSheetTheme.backgroundColor,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
                 ),
-                child: Material(
-                  color: Colors.transparent,
-                  child: Column(
-                    children: <Widget>[
-                      ListTile(
-                        contentPadding: EdgeInsets.symmetric(
-                          horizontal: 35,
-                          vertical: 10,
+              ),
+              child: Material(
+                color: Colors.transparent,
+                child: Column(
+                  children: <Widget>[
+                    ListTile(
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: 35,
+                        vertical: 8,
+                      ),
+                      leading: CircleAvatar(
+                        backgroundColor: Theme.of(context).primaryColor,
+                        child: Text(
+                          Globals.currentUser.name.substring(0, 1).toUpperCase(),
+                          style: TextStyle(color: Colors.white),
                         ),
-                        leading: CircleAvatar(
-                          backgroundColor: Theme.of(context).primaryColor,
-                          child: Text(
-                            Globals.currentUser.name.substring(0, 1).toUpperCase(),
-                            style: TextStyle(color: Colors.white),
+                        radius: 16,
+                      ),
+                      title: Text(Globals.currentUser.name),
+                      subtitle: Text(auth.currentUser.email),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ProfileView(),
                           ),
-                          radius: 16,
-                        ),
-                        title: Text(Globals.currentUser.name),
-                        subtitle: Text(auth.currentUser.email),
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => ProfileView(),
+                        );
+                      },
+                    ),
+                    Divider(
+                      height: 1,
+                      thickness: 1,
+                      color: Theme.of(context).accentColor.withOpacity(0.4),
+                    ),
+                    ListTile(
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: 35,
+                        vertical: 6,
+                      ),
+                      leading: Icon(Icons.settings),
+                      title: Text('Profile'),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => ProfileView(),
+                          ),
+                        );
+                      },
+                    ),
+                    ListTile(
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: 35,
+                        vertical: 6,
+                      ),
+                      leading: Icon(Icons.gavel),
+                      title: Text('Licenses'),
+                      onTap: () {
+                        showLicensePage(
+                          context: context,
+                          applicationName: 'CNECT',
+                          applicationVersion: Globals.packageInfo.version,
+                          applicationIcon: ConstrainedBox(
+                            constraints: BoxConstraints(
+                              minWidth: 120,
+                              minHeight: 120,
+                              maxWidth: 120,
+                              maxHeight: 120,
                             ),
-                          );
-                        },
-                      ),
-                      Divider(
-                        height: 1,
-                        thickness: 1,
-                        color: Theme.of(context).accentColor.withOpacity(0.4),
-                      ),
-                      ListTile(
-                        contentPadding: EdgeInsets.symmetric(
-                          horizontal: 16,
-                          vertical: 6,
-                        ),
-                        leading: Icon(Icons.settings),
-                        title: Text('Profile'),
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => ProfileView(),
+                            child: Image.asset(
+                              'assets/images/logos/logo.png',
+                              fit: BoxFit.fitHeight,
                             ),
-                          );
-                        },
+                          ),
+                          applicationLegalese: '2020-2021 FBLA Mobile Application project created by Mitchel Beeson, Noah Holoubek, and Emily Loseke.',
+                        );
+                      },
+                    ),
+                    ListTile(
+                      contentPadding: EdgeInsets.symmetric(
+                        horizontal: 35,
+                        vertical: 6,
                       ),
-                      ListTile(
-                        contentPadding: EdgeInsets.symmetric(
-                          horizontal: 16,
-                          vertical: 6,
-                        ),
-                        leading: Icon(Icons.gavel),
-                        title: Text('Licenses'),
-                        onTap: () {
-                          showLicensePage(
-                            context: context,
-                            applicationName: 'CNECT',
-                            applicationVersion: Globals.packageInfo.version,
-                            applicationIcon: ConstrainedBox(
-                              constraints: BoxConstraints(
-                                minWidth: 120,
-                                minHeight: 120,
-                                maxWidth: 120,
-                                maxHeight: 120,
-                              ),
-                              child: Image.asset(
-                                'assets/images/logos/logo.png',
-                                fit: BoxFit.fitHeight,
-                              ),
-                            ),
-                            applicationLegalese: 'FBLA Mobile Application project created by Mitchel Beeson, Noah Holoubek, and Emily Loseke.',
-                          );
-                        },
-                      ),
-                    ],
-                  ),
-                )
+                      leading: Icon(Icons.bug_report),
+                      title: Text('Report a Bug'),
+                      onTap: () {
+                        // TODO: Add bug report view
+                      },
+                    ),
+                  ],
+                ),
+              ),
             ),
           ),
         );
